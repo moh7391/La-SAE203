891478068e16ccc55afa8a1942b3273af725ab66
neomorisset <neomorisset@yahoo.fr>
2026-06-04 09:11:47 +0200

    sup de s inscrire

--- Visiteurs/header.php
+++ Visiteurs/header.php
@@ -24,7 +24,6 @@ function nav_active($nom, $active_page) {
   <nav class="client-nav" aria-label="Navigation principale">
     <a href="index.php"<?php echo nav_active('accueil', $active_page); ?>>Accueil</a>
     <a href="salles.php"<?php echo nav_active('salles', $active_page); ?>>Les Salles</a>
-    <a href="inscription.php"<?php echo nav_active('inscription', $active_page); ?>>S'inscrire</a>
     <a href="mon-espace.php"<?php echo nav_active('reservation', $active_page); ?>>Ma réservation</a>
     <a href="contact.php"<?php echo nav_active('contact', $active_page); ?>>Contact</a>
   </nav>
